7b1aa97d4fe057666689b306f778483e7d835686
Gabanelli <gabanelli.guilherme@gmail.com>
2023-04-01 11:11:04 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "hackaton_prod" {
+  identifier             = "hackatonprod"
+  engine                 = "mysql"
+  engine_version         = "8.0.23"
+  instance_class         = "db.t2.micro"
+  db_name                = "hackaton"
+  username               = "admin"
+  password               = "8r_FA29zLQ#^6n"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  backup_retention_period = 7
+  tags = {
+    Name = "My database"
+  }
+}
+
+output "endpoint" {
+  value = aws_db_instance.hackaton_prod.endpoint
+}
